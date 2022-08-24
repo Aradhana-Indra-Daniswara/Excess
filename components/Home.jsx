932a6429b603a702a235b0ee47adbcb87b94b28c
Aradhana Indra D(ev) <aradhanaindradev@gmail.com>
2022-08-24 20:21:25 +0700

    Added Icons

--- components/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Home() {
-    return <View>
-        tes123
-    </View>
-}
---
+++ components/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ScrollView, StyleSheet } from 'react-native'
+
+function Home() {
+  return(
+    <View>
+      <View >
+        
+      </View>
+      <ScrollView>
+
+      </ScrollView>
+      <View>
+
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
