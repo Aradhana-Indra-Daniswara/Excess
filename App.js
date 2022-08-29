f99045184a7ee86350eac5bbee4eaaf956790a8e
satosat <satriotimotius@gmail.com>
2022-08-29 21:59:52 +0700

    file restructuring

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
-import { useFonts } from 'expo-font'
-import PageNavigator from './pageNavigator'
 import React, { useCallback, useEffect } from 'react'
-import { Text } from 'react-native'
-import * as SplashScreen from 'expo-splash-screen';
+import PageNavigator from './components/PageNavigator'
 
 export default function App() {
   return (
