aa34d64bdb9a421dd59cb8533544bb66aa75446a
Aradhana Indra D(ev) <aradhanaindradev@gmail.com>
2022-08-30 15:16:32 +0700

    Rolled back App.js

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { NavigationContainer } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
 import PageNavigator from './pageNavigator'
 import React, { useCallback, useEffect } from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
+import { Text } from 'react-native'
 import * as SplashScreen from 'expo-splash-screen';
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar'
+
 export default function App() {
   return (
-    <Navbar />
+    <NavigationContainer>
+      <PageNavigator />
+    </NavigationContainer>
   )
 }
