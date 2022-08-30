a93a522ac9913448d0914e46bea39e2332e2a27b
Aradhana Indra D(ev) <aradhanaindradev@gmail.com>
2022-08-30 15:14:46 +0700

    Added basic navigation, updated homepage

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import { NavigationContainer } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
 import PageNavigator from './pageNavigator'
 import React, { useCallback, useEffect } from 'react'
-import { Text } from 'react-native'
+import { SafeAreaView, Text, View } from 'react-native'
 import * as SplashScreen from 'expo-splash-screen';
-
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar'
 export default function App() {
   return (
-    <NavigationContainer>
-      <PageNavigator />
-    </NavigationContainer>
+    <Navbar />
   )
 }
