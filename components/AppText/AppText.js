0da69efdda5a6e79c9829cf190e96905093c167a
Aradhana Indra D(ev) <aradhanaindradev@gmail.com>
2022-09-08 13:02:27 +0700

    Added AppText Component

---
+++ components/AppText/AppText.js
@@ -0,0 +1,24 @@
+import { Text } from 'react-native';
+import { useFonts } from 'expo-font';
+
+export default function AppText({ children, size, color, fontFamily }) {
+
+  const [fontsLoaded] = useFonts({
+    'Montserrat-Regular': require('../../assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Medium': require('../../assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-SemiBold': require('../../assets/fonts/Montserrat-SemiBold.ttf'),
+    'Montserrat-Bold': require('../../assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-Black': require('../../assets/fonts/Montserrat-Black.ttf'),
+    'Montserrat-Light': require('../../assets/fonts/Montserrat-Light.ttf'),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <Text style={{
+      fontFamily: fontFamily || 'Montserrat-Regular',
+      fontSize: size || 16,
+      color: color || 'black',
+    }}>{children}</Text>
+  )
+}
