e4eb87f00eb61fac408d83e0a2a19bb7ecf6fdac
Timotius Satrio Wicaksono <satriotimotius@gmail.com>
2022-09-22 14:17:53 +0700

    update qty works and renders correctly

--- components/Cart/Cart.js
+++ components/Cart/Cart.js
@@ -1,28 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { FlatList, Image, SafeAreaView, SectionList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import AppText from '../AppText'
-import ProductCard from './ProductCard'
-import formatCurrency from '../../utils/formatters/formatCurrency'
-import { SimpleLineIcons } from '@expo/vector-icons'; 
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import AppText from "../AppText";
+import ProductCard from "./ProductCard";
+import formatCurrency from "../../utils/formatters/formatCurrency";
+import { SimpleLineIcons } from "@expo/vector-icons";
 
 const Styles = StyleSheet.create({
   centerContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
   },
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     width: 250,
     height: 45,
     padding: 10,
     borderRadius: 10,
   },
   buttonFont: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontSize: 18,
-    fontWeight: '900',
+    fontWeight: "900",
   },
   shadow: {
     shadowColor: "#000",
@@ -32,143 +39,193 @@ const Styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 10
+    elevation: 10,
   },
   vendorInfoFont: {
-    fontWeight: '800',
+    fontWeight: "800",
     fontSize: 18,
-    color: '#59D9A8'
-  }
-})
+    color: "#59D9A8",
+  },
+});
 
 export default function Cart({ navigation }) {
-  
   // ini jadiin props dikasih dari vendorpage?
   const [ORDER_ITEMS, setORDER_ITEMS] = useState([
-    { vendor_id: 1, product_id: 1, product_name: 'Product 1', price: 30000, qty: 1 },
-    { vendor_id: 1, product_id: 2, product_name: 'Product 2', price: 40000, qty: 1 },
-    { vendor_id: 1, product_id: 3, product_name: 'Product 3', price: 50000, qty: 1 },
-    { vendor_id: 1, product_id: 4, product_name: 'Product 4', price: 60000, qty: 1 },
-    { vendor_id: 1, product_id: 5, product_name: 'Product 5', price: 60000, qty: 1 },
-    { vendor_id: 1, product_id: 6, product_name: 'Product 6', price: 60000, qty: 1 },
-  ])
+    {
+      vendor_id: 1,
+      product_id: 1,
+      product_name: "Product 1",
+      price: 30000,
+      qty: 1,
+    },
+    {
+      vendor_id: 1,
+      product_id: 2,
+      product_name: "Product 2",
+      price: 40000,
+      qty: 1,
+    },
+    {
+      vendor_id: 1,
+      product_id: 3,
+      product_name: "Product 3",
+      price: 50000,
+      qty: 1,
+    },
+    {
+      vendor_id: 1,
+      product_id: 4,
+      product_name: "Product 4",
+      price: 60000,
+      qty: 1,
+    },
+    {
+      vendor_id: 1,
+      product_id: 5,
+      product_name: "Product 5",
+      price: 60000,
+      qty: 1,
+    },
+    {
+      vendor_id: 1,
+      product_id: 6,
+      product_name: "Product 6",
+      price: 60000,
+      qty: 1,
+    },
+  ]);
 
   // ini juga jadiin props, jadiin satu aja sama ORDER_ITEMS(?)
   const VENDOR_DETAILS = {
     id: 1,
-    name: 'Salad Bowl',
-    address: 'BINUS Ave.',
+    name: "Salad Bowl",
+    address: "BINUS Ave.",
     openingHour: 8,
-    closingHour: 22
-  }
+    closingHour: 22,
+  };
 
   const [grandTotal, setGrandTotal] = useState(0);
 
   // Run once when page first load
   useEffect(() => {
-    const total = ORDER_ITEMS.reduce((prev, curr) => prev + curr.price * curr.qty, 0)
-    setGrandTotal(total)
-  }, [])
+    const total = ORDER_ITEMS.reduce(
+      (prev, curr) => prev + curr.price * curr.qty,
+      0
+    );
+    setGrandTotal(total);
+  }, [ORDER_ITEMS]);
 
   const deleteItem = (id) => {
-    const filteredItem = ORDER_ITEMS.filter(item => item.product_id !== id)
-    setORDER_ITEMS(filteredItem)
-  }
+    const filteredItem = [];
 
-  // probs need to refactor this, O(N)
-  const updateItem = (id, action) => {
-    const filteredItem = []
-    
+    ORDER_ITEMS.forEach((item) => {
+      if (item.product_id === id) {
+        setGrandTotal(grandTotal - item.price * item.qty);
+      } else {
+        filteredItem.push(item);
+      }
+    });
+
+    // console.log(filteredItem)
+    setORDER_ITEMS(filteredItem);
+  };
+
+  // probs need to refactor this
+  const updateItem = (id, qty) => {
+    const filteredItem = [];
+
     // search for item, push any other item directly to filteredItem
     ORDER_ITEMS.forEach((product) => {
-      if(product.product_id !== id) {
-        filteredItem.push(product)
-      } else {
-        // Update item's qty and update grand total to only subtract/add item's price
-        switch(action) {
-          case "increment":
-            product.qty += 1
-            setGrandTotal(grandTotal + product.price)
-            break
-          case "decrement":
-            product.qty > 1 
-              ? product.qty -= 1
-              : null
-            setGrandTotal(grandTotal - product.price)
-            break
-        }
-        filteredItem.push(product)
+      if (product.product_id === id) {
+        product.qty = qty;
       }
-    })
+
+      filteredItem.push(product);
+    });
 
     // renew ORDER_ITEMS w/ all items
     setORDER_ITEMS(filteredItem);
-  }
+  };
 
-  const renderItem = ({item}) => {
+  const renderItem = ({ item }) => {
     return (
-      <ProductCard 
-        productId={item.product_id}
-        productName={item.product_name}
-        productPrice={item.price}
-        productImageId={`${item.vendor_id}_${item.product_id}`}
-        // productQty={item.qty}
+      <ProductCard
+        product={item}
         updateItem={updateItem}
+        deleteItem={deleteItem}
       />
-    )
-  }
+    );
+  };
 
   const renderSeparatorComponent = () => {
     return (
-      <View style={{ borderTopWidth: 1, borderColor: '#C2C2C2', marginTop: 10 }}></View>
-    )
-  }
+      <View
+        style={{ borderTopWidth: 1, borderColor: "#C2C2C2", marginTop: 10 }}
+      ></View>
+    );
+  };
 
-  const renderFooterComponent = () => {
-    return (
-      <View style={[Styles.centerContainer, { marginVertical: 20 }]}>
-        <AppText fontFamily="Montserrat-SemiBold" size={18} >
+  return (
+    <SafeAreaView style={[Styles.centerContainer, { marginTop: 10 }]}>
+      {/* Vendor Information */}
+      <View style={{ alignItems: "flex-start" }}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <SimpleLineIcons
+            name="clock"
+            size={20}
+            color="black"
+            style={{ marginRight: 6 }}
+          />
+          <AppText weight={"600"} style={{ fontSize: 16 }}>
+            {VENDOR_DETAILS.openingHour}.00 - {VENDOR_DETAILS.closingHour}.00
+          </AppText>
+        </View>
+        <AppText style={{ color: "#59D9A8", fontSize: 18 }} weight={"700"}>
+          {VENDOR_DETAILS.name}, {VENDOR_DETAILS.address}
+        </AppText>
+      </View>
+
+      {/* Items in cart */}
+      <FlatList
+        data={ORDER_ITEMS}
+        renderItem={renderItem}
+        ItemSeparatorComponent={renderSeparatorComponent}
+        showsVerticalScrollIndicator={false}
+        style={{
+          marginBottom: 120,
+        }}
+        keyExtractor={({ product_id }) => product_id}
+      />
+
+      {/* Footer */}
+      <View
+        style={[
+          Styles.centerContainer,
+          {
+            marginVertical: 20,
+            position: "absolute",
+            bottom: 20,
+            height: 100,
+          },
+        ]}
+      >
+        <AppText weight={"700"} style={{ fontSize: 18 }}>
           Grand Total: {formatCurrency(grandTotal)}
         </AppText>
-        <TouchableOpacity 
-          style={[Styles.button, Styles.shadow, { 
-            backgroundColor: '#51C699', 
-            marginTop: 20,
-          }]}
+        <TouchableOpacity
+          style={[
+            Styles.button,
+            Styles.shadow,
+            {
+              backgroundColor: "#51C699",
+              marginTop: 20,
+            },
+          ]}
           onPress={() => navigation.navigate("Checkout")}
         >
           <Text style={[Styles.buttonFont]}>Checkout</Text>
         </TouchableOpacity>
       </View>
-    )
-  }
-  
-  return (
-    <SafeAreaView style={[Styles.centerContainer, { marginTop: 10 }]}>
-
-      {/* Vendor Information */}
-      <View style={{ alignItems: 'flex-start' }} > 
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <SimpleLineIcons name="clock" size={20} color="black" style={{ marginRight: 6 }} />
-          <AppText fontFamily="Montserrat-SemiBold" size={16} >
-            {VENDOR_DETAILS.openingHour}.00 - {VENDOR_DETAILS.closingHour}.00
-          </AppText>
-        </View>
-        <AppText fontFamily="Montserrat-Bold" size={18} color="#59D9A8">
-          {VENDOR_DETAILS.name}, {VENDOR_DETAILS.address}
-        </AppText>
-      </View>
-
-      {/* Products scroll view */}
-      <View style={[Styles.centerContainer, { height: 600 }]}>
-        <FlatList
-          data={ORDER_ITEMS}
-          renderItem={renderItem}
-          ItemSeparatorComponent={renderSeparatorComponent}
-          ListFooterComponent={renderFooterComponent}
-          showsVerticalScrollIndicator={false}
-        />
-      </View>
     </SafeAreaView>
-  )
+  );
 }
--- components/Cart/ProductCard.js
+++ components/Cart/ProductCard.js
@@ -1,73 +1,97 @@
-import React, { useState } from 'react'
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
-import { AntDesign, Ionicons } from '@expo/vector-icons'; 
-import AppText from '../AppText';
-import formatCurrency from '../../utils/formatters/formatCurrency';   
+import React, { useEffect, useState } from "react";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import AppText from "../AppText";
+import formatCurrency from "../../utils/formatters/formatCurrency";
 
 const Styles = StyleSheet.create({
   cardContainer: {
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
     height: 130,
     width: 310,
-    borderColor: '#C2C2C2',
+    borderColor: "#C2C2C2",
   },
   centerContainer: {
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   qtyContainer: {
     width: 50,
     height: 35,
     borderRadius: 20,
     borderWidth: 1,
-    borderColor: '#C2C2C2'
-  }
-})
+    borderColor: "#C2C2C2",
+  },
+});
 
-export default function ProductCard({ productId, productName, productPrice, productImageId, updateItem }) {
+export default function ProductCard({ product, updateItem, deleteItem }) {
   const [qty, setQty] = useState(1);
 
   const qtyHandler = (action) => {
-    switch(action) {
+    switch (action) {
       case "increment":
-        setQty(qty + 1)
-        updateItem(productId, action)
-        break
+        setQty(qty + 1);
+        break;
       case "decrement":
-        if(qty > 1) {
-          setQty(qty - 1)
-          updateItem(productId, action)
-          break
+        if (qty > 1) {
+          setQty(qty - 1);
+          break;
         }
     }
-  }
+  };
+
+  useEffect(() => {
+    updateItem(product.product_id, qty);
+  }, [qty]);
 
   return (
-    <View style={[Styles.cardContainer, { }]}>
+    <View style={[Styles.cardContainer]}>
       <View style={{ marginTop: 16 }}>
-        <View style={{ flexDirection: 'row' }}>
+        <View style={{ flexDirection: "row" }}>
           <View>
-            <View style={{ flexDirection: 'row',  alignItems: 'center' }}>
-              <AppText fontFamily="Montserrat-SemiBold" size={16} >{productName}</AppText>
-              <Pressable style={{ marginLeft: 10, height: 30, width: 40, alignItems: 'center' }} >
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <AppText weight={"600"} style={{ fontSize: 18 }}>
+                {product.product_name}
+              </AppText>
+              <Pressable
+                style={{
+                  marginLeft: 10,
+                  height: 30,
+                  width: 40,
+                  alignItems: "center",
+                }}
+                onPress={() => deleteItem(product.product_id)}
+              >
                 <Ionicons name="md-trash-outline" size={28} color="black" />
               </Pressable>
             </View>
-            <AppText fontFamily="Montserrat-Regular" size={14}>{formatCurrency(productPrice)}</AppText>
+            <AppText weight={"300"} style={{ fontSize: 15 }}>
+              {formatCurrency(product.price)}
+            </AppText>
           </View>
         </View>
-        
+
         {/* QTY BTN */}
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between',  alignItems: 'center',  width: 140, marginTop: 18 }}>
-          <Pressable onPress={() => qtyHandler('decrement')} >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: 140,
+            marginTop: 18,
+          }}
+        >
+          <Pressable onPress={() => qtyHandler("decrement")}>
             <AntDesign name="minuscircle" size={35} color="black" />
           </Pressable>
           <View style={[Styles.centerContainer, Styles.qtyContainer]}>
-            <AppText fontFamily={"Montserrat-Regular"}>{qty}</AppText>
+            <AppText weight={"400"} style={{ fontSize: 15 }}>
+              {qty}
+            </AppText>
           </View>
-          <Pressable onPress={() => qtyHandler('increment')} >
+          <Pressable onPress={() => qtyHandler("increment")}>
             <AntDesign name="pluscircle" size={35} color="black" />
           </Pressable>
         </View>
@@ -75,8 +99,8 @@ export default function ProductCard({ productId, productName, productPrice, prod
 
       {/* product image */}
       <View>
-        <Image source={require('../../assets/1-1.png')} />
+        <Image source={require("../../assets/1-1.png")} />
       </View>
     </View>
-  )
+  );
 }
