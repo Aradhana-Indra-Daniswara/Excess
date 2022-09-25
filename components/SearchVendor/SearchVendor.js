2eca5b94fca9de55abaea721fbdabce734175860
Aradhana Indra Daniswara <aradhanaindradev@gmail.com>
2022-09-25 14:13:48 +0700

    SearchVendor updated

--- components/SearchVendor/SearchVendor.js
+++ components/SearchVendor/SearchVendor.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, SafeAreaView, TextInput, View } from 'react-native';
+import { FlatList, SafeAreaView, TextInput, TouchableOpacity, View } from 'react-native';
 import SearchIcon_Gray from '../../assets/search_icon_gray.svg';
 import CrossIcon_Gray from '../../assets/cross_icon_gray.svg';
+import Back_button from '../../assets/Back_button.svg';
 import { colorStyles } from '../Styling/GlobalStyles';
 import { useFonts } from 'expo-font';
 import VendorList from './VendorList';
 import { firestore, storage } from '../../config/firebase-config';
 import { collection, doc, getDocs } from "firebase/firestore";
+import { useNavigation } from '@react-navigation/native';
+
 
 export default function SearchVendor() {
   const [fontsLoaded] = useFonts({
     'OpenSauceSans-Regular': require('../../assets/fonts/OpenSauceSans-Regular.ttf'),
   });
+  const navigation = useNavigation();
   const placeholder = 'Cheap Snack?'
   const [isLoading, setIsLoading] = useState(true);
   const [vendors, setVendors] = useState();
@@ -37,36 +41,49 @@ export default function SearchVendor() {
     return null;
   }
   return (
-    <SafeAreaView>
-      <View style={{
-        height: 40,
-        backgroundColor: 'white',
-        width: '80%',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderColor: colorStyles[80],
-        borderRadius: 5,
-        paddingHorizontal: 10
-      }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <SearchIcon_Gray />
-          <TextInput style={{
-            height: '100%',
-            width: '85%',
-            fontSize: 16,
-            fontFamily: 'OpenSauceSans-Regular',
-            marginLeft: 10,
-          }}
-            placeholder={placeholder} />
+    <SafeAreaView style={{
+      backgroundColor: 'white',
+      height: '100%',
+    }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 10, marginHorizontal: 16 }}>
+        <TouchableOpacity onPress={()=>{
+          navigation.navigate('Home')
+        }}>
+          <Back_button />
+        </TouchableOpacity>
+        <View style={{
+          marginLeft: 16,
+          height: 40,
+          backgroundColor: 'white',
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignSelf: 'center',
+          alignItems: 'center',
+          borderWidth: 1,
+          borderColor: colorStyles[80],
+          borderRadius: 5,
+          paddingHorizontal: 10
+        }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <SearchIcon_Gray />
+            <TextInput style={{
+              height: '100%',
+              width: '85%',
+              fontSize: 16,
+              fontFamily: 'OpenSauceSans-Regular',
+              marginLeft: 10,
+            }}
+              placeholder={placeholder} />
+          </View>
+          <CrossIcon_Gray />
         </View>
-        <CrossIcon_Gray />
       </View>
+
       <FlatList
         data={vendors}
         keyExtractor={(item) => item.id}
-        renderItem={object => <VendorList item={object.item} />}
+        renderItem={vendor => <VendorList vendor={vendor.item} />}
       />
     </SafeAreaView>
   );
--- components/SearchVendor/VendorList.js
+++ components/SearchVendor/VendorList.js
@@ -4,54 +4,82 @@ import { firestore, storage } from '../../config/firebase-config';
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { collection, doc, getDoc } from "firebase/firestore";
 import AppText from '../AppText';
+import { colorStyles } from '../Styling/GlobalStyles';
+import Star_icon from '../../assets/star_icon.svg';
 
-export default function VendorList({ item }) {
+export default function VendorList({ vendor }) {
   const [promoAmount, setPromoAmount] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
+  const [vendorIconURI, setVendorIconURI] = useState();
 
   const countPromoAmount = async () => {
     try {
-      const docRef = doc(firestore, "vendors", item.id);
+      const docRef = doc(firestore, "vendors", vendor.id);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
-        const data = docSnap.data().products;
-        console.log(data)
-        const promoAmount = data.filter((item) => item.limited_time === true).length
+        const vendorProducts = docSnap.data().products;
+        const promoAmount = vendorProducts.filter((product) => product.limited_time === true).length
         setPromoAmount(promoAmount)
       }
-      setIsLoading(false);
     }
     catch (e) {
-      console.log(e.message);
+      console.log("countPromoAmount() " + e.message);
     }
   }
 
+  const fetchVendorImage = async () => {
+    const docRef = doc(firestore, "vendors", vendor.id);
+
+    try {
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        const vendorImageUrl = await getDownloadURL(ref(storage, vendor.logo))
+        setVendorIconURI(vendorImageUrl);
+      }
+    }
+    catch (error) {
+      console.log("fetchVendorImage() " + error.message)
+    }
+  }
+
+  const loadVendorData = async () => {
+    await countPromoAmount();
+    await fetchVendorImage();
+    setIsLoading(false)
+  }
   useEffect(() => {
-    countPromoAmount()
-    console.log(item.id)
+    loadVendorData();
   }, [])
+
   if (isLoading) {
     return <AppText>Loading</AppText>;
   }
 
   return (
     <View style={{
-      paddingVertical: 4,
-      flexDirection: 'row'
+      flexDirection: 'row',
+      marginVertical: 4,
+      marginHorizontal: 16
     }}>
-      <Image />
-      <View>
+      <Image source={{ uri: vendorIconURI }} style={{ width: 90, height: 90, marginRight: 8 }} />
+      <View style={{ flex: 1, marginVertical: 10 }}>
         <View>
-          <AppText>
+          <AppText style={{ color: colorStyles['excess'], fontSize: 12 }} weight='700'>
             {promoAmount} items on promo
           </AppText>
-          <AppText>
-            {item.name}, {item.area}
+          <AppText style={{ color: colorStyles['20'], fontSize: 14 }} weight='700'>
+            {vendor.name} - {vendor.area}
           </AppText>
         </View>
-        <View>
-          <AppText>4.9</AppText>
-          <AppText>2.1 km</AppText>
+        <View style={{
+          flexDirection: 'row',
+          marginTop: 4
+        }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center'}}>
+            <AppText style={{ color: colorStyles['50'], fontSize: 12, marginRight: 4 }}>4.9</AppText>
+            <Star_icon />
+          </View>
+          <AppText style={{ color: colorStyles['50'], fontSize: 12 }}> • 2.1 km</AppText>
         </View>
       </View>
     </View>
