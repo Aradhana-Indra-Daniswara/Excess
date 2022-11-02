d7dad66dae1eb6d652d1e5cdf32d7b20360e881b
Timotius Satrio Wicaksono <satriotimotius@gmail.com>
2022-11-03 01:10:41 +0700

    used discounted_price

--- components/vendorpage/ProductCard.js
+++ components/vendorpage/ProductCard.js
@@ -6,45 +6,48 @@ import { colorStyles } from "../Styling/GlobalStyles";
 import formatCurrency from "../../utils/formatters/formatCurrency";
 
 export default function ProductCard({ product, cartHandler }) {
-	return (
-		<View
-			style={{
-				flexDirection: "row",
-				borderWidth: 1,
-				borderColor: "#F2F2F2",
-				borderRadius: 5,
-				padding: 5,
-				marginVertical: 4,
-				justifyContent: "space-between",
-				marginHorizontal: 16,
-			}}>
-			<View style={{ flexDirection: "row", alignItems: "center", width: 150 }}>
-				<View>
-					<Image
-						source={{ uri: product.imageUrl }}
-						style={{ width: 72, height: 72, marginRight: 8, borderRadius: 5 }}
-					/>
-				</View>
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        borderWidth: 1,
+        borderColor: "#F2F2F2",
+        borderRadius: 5,
+        padding: 5,
+        marginVertical: 4,
+        justifyContent: "space-between",
+        marginHorizontal: 16,
+      }}
+    >
+      <View style={{ flexDirection: "row", alignItems: "center", width: 150 }}>
+        <View>
+          <Image
+            source={{ uri: product.imageUrl }}
+            style={{ width: 72, height: 72, marginRight: 8, borderRadius: 5 }}
+          />
+        </View>
 
-				<View style={{ marginLeft: 10 }}>
-					<AppText
-						style={{ fontSize: 14 }}
-						weight='500'>
-						{product.name}
-					</AppText>
-					<AppText style={{ fontSize: 14, marginTop: 8 }}>{formatCurrency(product.price)}</AppText>
-				</View>
-			</View>
-			<TouchableOpacity
-				style={{ flexDirection: "row", alignItems: "center" }}
-				onPress={() => cartHandler(product)}>
-				<AppText
-					style={{ marginRight: 4, color: colorStyles["excess"], fontSize: 14 }}
-					weight='500'>
-					Add to Cart
-				</AppText>
-				<Right_arrow_icon />
-			</TouchableOpacity>
-		</View>
-	);
+        <View style={{ marginLeft: 10 }}>
+          <AppText style={{ fontSize: 14 }} weight="500">
+            {product.name}
+          </AppText>
+          <AppText style={{ fontSize: 14, marginTop: 8 }}>
+            {formatCurrency(product.discounted_price)}
+          </AppText>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={{ flexDirection: "row", alignItems: "center" }}
+        onPress={() => cartHandler(product)}
+      >
+        <AppText
+          style={{ marginRight: 4, color: colorStyles["excess"], fontSize: 14 }}
+          weight="500"
+        >
+          Add to Cart
+        </AppText>
+        <Right_arrow_icon />
+      </TouchableOpacity>
+    </View>
+  );
 }
